e24c20d69142ea030be3bbc9ba242c808c0e5448
paoloprogram12 <sethpsalazar@gmail.com>
2026-01-05 13:59:10 -0800

    fixed one way bug

--- flight_price_tracker/src/web/static/script.js
+++ flight_price_tracker/src/web/static/script.js
@@ -152,14 +152,14 @@ const returnDateGroup = document.getElementById('return_date_group');
 tripTypeInputs.forEach(input => {
     input.addEventListener('change', function() {
         if (this.value === 'one-way') {
-            returnDateGroup.disabled = true;
-            returnDateGroup.required = false;
-            returnDateGroup.value = '';
+            returnDateInput.disabled = true;
+            returnDateInput.required = false;
+            returnDateInput.value = '';
             returnDateGroup.style.display = 'none';
             
         } else {
-            returnDateGroup.disabled = false;
-            returnDateGroup.required = true;
+            returnDateInput.disabled = false;
+            returnDateInput.required = true;
             returnDateGroup.style.display = 'block';
         }
     });
